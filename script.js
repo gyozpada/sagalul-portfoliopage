29e30731c456b3e2c37806effa69a09c764f4862
ahmad galang satria <ahmadgalangclass@gmail.com>
2020-12-03 19:54:36 +0700

    update a.1

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-console.log('its working')
+const splash = document.querySelector('.splash');
 
-let theme = localStorage.getItem('theme')
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    splash.classList.add('display-none');
+  }, 2000);
+});
 
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
+let theme = localStorage.getItem('theme');
+
+if (theme == null) {
+  setTheme('light');
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName('theme-dot');
 
-for (var i =0; themeDots.length > i; i++) {
- themeDots[i].addEventListener('click', function(){
- 	let mode = this.dataset.mode
- 	console.log('Option Clicked', mode)
- 	setTheme(mode)
- })
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener('click', function () {
+    let mode = this.dataset.mode;
+    console.log('Option Clicked', mode);
+    setTheme(mode);
+  });
 }
 
 function setTheme(mode) {
-	if(mode=='light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
+  if (mode == 'light') {
+    document.getElementById('theme-style').href = 'default.css';
+  }
 
-	if(mode=='blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
+  if (mode == 'blue') {
+    document.getElementById('theme-style').href = 'blue.css';
+  }
 
-	if(mode=='green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
+  if (mode == 'green') {
+    document.getElementById('theme-style').href = 'green.css';
+  }
 
-	if(mode=='purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
+  if (mode == 'purple') {
+    document.getElementById('theme-style').href = 'purple.css';
+  }
 
-
-	localStorage.setItem('theme', mode)
+  localStorage.setItem('theme', mode);
 }
